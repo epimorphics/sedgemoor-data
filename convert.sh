94859bd7398d67bb2ef6ea7005fe8022d02941c1
Brian McBride <brian@epimorphics.com>
2014-12-12 11:34:47 +0000

    Switch to using yaml again

--- convert.sh
+++ convert.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
 # Use DCLib to convert a CSV file
-DCLIB_VERSION=0.0.5
-TEMPFILE=`mktemp`
-cat data/csv/$1.csv > $TEMPFILE
-echo $TEMPFILE
-cat $TEMPFILE
-echo ===============================================
-java -jar lib/dclib-$DCLIB_VERSION-run.jar --debug templates/$1.json data/csv/$1.csv
-rm $TEMPFILE
+DCLIB_VERSION=0.0.6-SNAPSHOT
+
+mkdir -p $1/rdf
+for FILEPATH in $1/csv/*.csv
+do
+  FILE=$(basename $FILEPATH)
+  ROOT=${FILE%.csv}  
+  java -jar lib/dclib-$DCLIB_VERSION-run.jar --debug templates/$ROOT.yaml $1/csv/$ROOT.csv > $1/rdf/$ROOT.ttl
+done
