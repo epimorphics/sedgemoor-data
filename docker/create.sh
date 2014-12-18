ab5600ef39f7a53babe303878cd412cb6421da57
Brian McBride <brian@epimorphics.com>
2014-12-18 14:24:20 +0000

    Added webpage-demo.html and updated index.html

--- docker/create.sh
+++ docker/create.sh
@@ -5,7 +5,7 @@ sudo docker run -d --name sedgemoor \
      -v `pwd`/fs/etc/fuseki/:/etc/fuseki/ \
      -v `pwd`/fs/etc/elda/conf.d:/etc/elda/conf.d \
      -v `pwd`/fs/etc/apache2/sites-available:/etc/apache2/sites-available \
-     -v `pwd`/fs/var/www/data.sedgemoor.gov.uk/html/lda-assets:/var/www/data.sedgemoor.gov.uk/html/lda-assets \
+     -v `pwd`/fs/var/www/data.sedgemoor.gov.uk/html/:/var/www/data.sedgemoor.gov.uk/html/ \
      -p 80:80 \
      -p 3030:3030 \
      -p 8080:8080 \
